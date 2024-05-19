53ff3be042db8694d8054ea0b47dfe6f5aafe003
MrZorg111 <yodji333@mail.ru>
2024-05-19 20:34:32 +0300

    Добавьте файлы проекта.

---
+++ UEHomework_15.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+void OutputEvenNumbers(int finder, const int stopFind) {
+	for (int i = 0; i < stopFind/2; i++) {
+		std::cout << finder << std::endl;
+		finder += 2;
+	}
+}
+
+
+
+int main(){
+	int first_num = 2;
+	const int N = 30;
+
+
+	// Output of even or odd numbers!
+	OutputEvenNumbers(first_num, N);
+	std::cout << "\n\n";
+	OutputEvenNumbers(first_num - 1, N);
+}
